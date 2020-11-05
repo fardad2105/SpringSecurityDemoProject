298ea7e4ae73afbe72d8f30b0cf2b30f69c5b226
fardad2105 <fardadyadegare@yahoo.com>
2020-11-05 13:26:58 +0330

    Ant Matchers

--- src/main/java/com/fy/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/fy/springsecurity/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/","index","/css/*","/js/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
