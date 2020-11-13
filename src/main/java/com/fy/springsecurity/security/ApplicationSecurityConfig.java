f252670fa3c5197796a0c90c515348e913fa2621
fardad2105 <fardadyadegare@yahoo.com>
2020-11-13 22:12:11 +0330

    create jwt token base username and password Authentication Request and filter

--- src/main/java/com/fy/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/fy/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,19 +1,16 @@
 package com.fy.springsecurity.security;
 
+import com.fy.springsecurity.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
@@ -26,10 +23,13 @@ import static com.fy.springsecurity.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService; // Database Auth
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
+                                     ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -69,34 +69,53 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutSuccessUrl("/login");
     }
 
+    // this below method used for Memory base Authentication
+
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//       UserDetails nedaUser  = User.builder()
+//                .username("neda")
+//                .password(passwordEncoder.encode("password"))
+////                .roles(STUDENT.name()) // ROLE_STUDENT
+//                .authorities(STUDENT.getGrantedAuthorities())
+//                .build();
+//
+//       UserDetails aliUser = User.builder()
+//               .username("ali")
+//               .password(passwordEncoder.encode("password123"))
+////               .roles(ADMIN.name())  // ROLE_ADMIN
+//               .authorities(ADMIN.getGrantedAuthorities())
+//               .build();
+//
+//        UserDetails nimaUser = User.builder()
+//                .username("nima")
+//                .password(passwordEncoder.encode("password123"))
+////                .roles(ADMINTRAINEE.name())  // ROLE_ADMINTRAINEE
+//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+//                .build();
+//
+//       return new InMemoryUserDetailsManager(
+//               nedaUser,
+//               aliUser,
+//               nimaUser
+//       );
+//    }
+
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-       UserDetails nedaUser  = User.builder()
-                .username("neda")
-                .password(passwordEncoder.encode("password"))
-//                .roles(STUDENT.name()) // ROLE_STUDENT
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-       UserDetails aliUser = User.builder()
-               .username("ali")
-               .password(passwordEncoder.encode("password123"))
-//               .roles(ADMIN.name())  // ROLE_ADMIN
-               .authorities(ADMIN.getGrantedAuthorities())
-               .build();
-
-        UserDetails nimaUser = User.builder()
-                .username("nima")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMINTRAINEE.name())  // ROLE_ADMINTRAINEE
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-       return new InMemoryUserDetailsManager(
-               nedaUser,
-               aliUser,
-               nimaUser
-       );
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+
+
+    // this below method used for Database  Authentication
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
 }
